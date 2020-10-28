2522fd8462ba7e8dd673f722fd1b4a6335c7835f
matin-binary <matin@deriv.com>
2020-10-28 17:35:34 +0800

    Matin/Comment: Added comments to justify the latest changes

--- src/javascript/_common/base/client_base.js
+++ src/javascript/_common/base/client_base.js
@@ -269,6 +269,8 @@ const ClientBase = (() => {
         let type;
         if ((upgradeable_landing_companies || []).length) {
             const current_landing_company = get('landing_company_shortcode');
+            // create multiple accounts only available for landing companies with legal_allowed_currencies
+            // changes duo to putting clients risiding in Rwanada inside unwelcome logins list
             can_open_multi = upgradeable_landing_companies.indexOf(current_landing_company) !== -1 &&
                              (landing_company_obj && landing_company_obj.legal_allowed_currencies);
 
