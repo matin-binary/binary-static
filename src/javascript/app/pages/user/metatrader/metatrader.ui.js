69a11659a5d2ea4ba0978ab1d9ce2532e7d5c257
Mohammadreza Ghorbani <reza@firstsource.email>
2021-04-19 15:52:10 +0430

    Fix eslint

--- src/javascript/app/pages/user/metatrader/metatrader.ui.js
+++ src/javascript/app/pages/user/metatrader/metatrader.ui.js
@@ -942,7 +942,7 @@ const MetaTraderUI = (() => {
             if (shouldSetTradingPassword()) {
                 button_selector = '#new_user_btn_submit_new_account';
             }
-             button_selector = '#existing_user_btn_submit_new_account';
+            button_selector = '#existing_user_btn_submit_new_account';
         }
         return actions_info[action].$form.find(button_selector);
     };
